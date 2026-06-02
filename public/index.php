5558f03ccd926dd17b1de46e709946ef030c163f
DANNON Mahugnon Nellys Keneth <nellysdannon@gmail.com>
2026-06-02 14:31:37 +0200

    réparation de index.php

--- public/index.php
+++ public/index.php
@@ -3,6 +3,11 @@ require_once "../config/database.php";
 require_once "../app/controllers/dashboardController.php";
 require_once "../app/controllers/PublicationController.php";
 
+require_once "../config/database.php";
+
+$database = new Database();
+$pdo = $database->connect(); // Initialise $pdo pour les modèles et requêtes
+
 // niveau choisi
 $niveau = $_GET['niveau'] ?? "Tous";
 
