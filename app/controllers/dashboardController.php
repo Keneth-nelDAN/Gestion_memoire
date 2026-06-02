e1ad5c3cbc9a92112ab6586e7f52fb2f266fd047
DANNON Mahugnon Nellys Keneth <nellysdannon@gmail.com>
2026-06-02 15:20:41 +0200

    misee dashboardController.php

--- app/controllers/dashboardController.php
+++ app/controllers/dashboardController.php
@@ -2,7 +2,7 @@
 require_once "../config/database.php";
 require_once "../app/models/dashboard.php";
 require_once "../app/models/memoire.php";
-require_once "../app/models/Publication.php";
+require_once "../app/models/publication.php";
 require_once "../app/models/like.php";
 
 class dashboardController {
